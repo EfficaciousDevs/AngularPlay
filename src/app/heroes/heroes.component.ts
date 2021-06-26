fc1663ca2178016e2f78fd07ff0969150ff4c8d8
Akshay <aman.akshay88@gmail.com>
2021-06-26 19:38:50 +0530

    Initial Commit: Angular Files.

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HEROES } from './mock-heroes';
+import {Hero} from "./hero";
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Mahendra Singh Dhoni'
+  // };
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
---
+++ src/app/heroes/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Sachin Tendulkar' },
+  { id: 2, name: 'Virendra Sehwag' },
+  { id: 3, name: 'Sourav Ganguly' },
+  { id: 4, name: 'Rahul Dravid' },
+  { id: 5, name: 'Vvs Laxman' },
+  { id: 6, name: 'Yuvraj Singh' },
+  { id: 7, name: 'Mahendra Singh Dhoni' },
+  { id: 8, name: 'Virat Kohli' },
+  { id: 9, name: 'Rohit Sharma' },
+  { id: 10, name: 'Suresh Raina' }
+];
